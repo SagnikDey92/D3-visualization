2d557d1d1e3fd90250cc2611dcb11b2dfce8171f
Deepcharran <51226116+Deepcharran@users.noreply.github.com>
2019-07-01 18:39:45 +0530

    First Commit

--- open.js
+++ open.js
@@ -20,9 +20,9 @@ fileInp.addEventListener('change', function (evnt)		/*-----Commands to be perfor
                      .append("svg")
                      .attr("width", 500)
       	             .attr("height", 40)
-                     .attr("transform","translate(200,5)")
+                     .attr("transform","translate(350,-30)")
                      .append("text")
- 		     .attr("transform","translate(250,30)")
+ 		     .attr("transform","translate(80,30)")
                      .style("text-anchor","middle")
                      .style("font-size","30px")
                      .text("Pie Charts");    
